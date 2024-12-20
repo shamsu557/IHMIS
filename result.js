ecca0afe22d6a3075be3497f99c117b4e193beae
shamsu557 <118239877+shamsu557@users.noreply.github.com>
2024-12-20 19:11:31 +0100

    Add files via upload

--- result.js
+++ result.js
@@ -348,3 +348,7 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+  // Close button functionality
+  function goBack() {
+    window.history.back();
+    }
