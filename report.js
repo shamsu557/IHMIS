bf1102483db98084a818aaf6b9510ce31a2f0570
shamsu557 <118239877+shamsu557@users.noreply.github.com>
2024-12-26 15:39:48 +0100

    Add files via upload

--- report.js
+++ report.js
@@ -93,3 +93,19 @@ fetch('/stats')
         alert('Failed to load stats.');
     });
 });
+
+    // Back to top button functionality
+    window.onscroll = function() { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
+
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
